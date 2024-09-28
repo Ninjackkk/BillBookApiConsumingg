a06b7dddb98339606c68fe58f25e64377538ccae
usaama <usaama@LAPTOP>
2024-09-28 21:19:45 +0530

    Add project files.

---
+++ Controllers/PurchaseController.cs
@@ -0,0 +1,93 @@
+﻿using Azure;
+using BillBookApiConsuming.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace BillBookApiConsuming.Controllers
+{
+    public class PurchaseController : Controller
+    {
+        HttpClient client;                      //Declaring global object of HttpClient class 
+
+        public PurchaseController()
+        {
+            HttpClientHandler clientHandler = new HttpClientHandler();
+            clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+
+            client = new HttpClient(clientHandler);        // loading the client handler into client object
+
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var categories = await GetAllCategoriesAsync();
+           
+            return View(categories);
+        }
+
+        private async Task<List<Category>> GetAllCategoriesAsync()
+        {
+            string url = "https://localhost:44381/api/Purchase/GetAllCategories";
+            var response = await client.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var jsondata = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<List<Category>>(jsondata);
+            }
+            return new List<Category>(); // Return an empty list on failure
+        }
+
+        [HttpGet]
+        public async Task<JsonResult> GetItemsByCategoryId(int categoryId)
+        {
+            string url = $"https://localhost:44381/api/Purchase/GetItemsByCategoryId/{categoryId}";
+            var response = await client.GetAsync(url);
+            List<Inventories> itemList = new List<Inventories>();
+
+            if (response.IsSuccessStatusCode)
+            {
+                var jsondata = await response.Content.ReadAsStringAsync();
+                itemList = JsonConvert.DeserializeObject<List<Inventories>>(jsondata);
+            }
+
+            return Json(itemList); // Return JSON data
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> NewPurchase(PurchaseRequest purchaseRequest)
+        {
+            if (purchaseRequest == null)
+            {
+                return BadRequest("Invalid purchase request.");
+            }
+            purchaseRequest.PurchaseOrder.PurchaseDate = DateTime.Now;
+            purchaseRequest.PurchaseOrder.Status = "Unpaid";
+            purchaseRequest.PurchaseOrder.BusinessId = 1001;// we have to take through session
+            // API call to create a new purchase order
+            using (var apiClient = new HttpClient())
+            {
+                var jsonRequest = JsonConvert.SerializeObject(purchaseRequest);
+                var content = new StringContent(jsonRequest, Encoding.UTF8, "application/json");
+                var result = await apiClient.PostAsync("https://localhost:44381/api/Purchase/NewPurchase", content);
+
+                if (result.IsSuccessStatusCode)
+                {
+                    return Ok("Purchase order created successfully.");
+                }
+
+                return BadRequest("Error creating purchase order.");
+            }
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using BillBookApiConsuming.Models;
+namespace BillBookApiConsuming.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<PurchaseOrder> PurchaseOrders { get; set; }
+        public DbSet<MyStock> MyStocks { get; set; }
+
+        public DbSet<Inventories> Inventories { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
